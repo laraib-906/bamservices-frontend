98f8da8213790f6f992f836104760b7b87b9b731
laraib-906 <laraibamna906@gmail.com>
2021-11-04 00:49:57 +0500

    Updated fetch Request

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,7 +6,5 @@ export async function login(): Promise<any> {
 }
 
 export async function signup(requestPayload: any): Promise<any> {
-    const query = objToQuery(requestPayload);
-    console.log(config.authAPI);
-    return _fetch(`${config.authAPI}`, 'POST', JSON.stringify(query));
+    return _fetch(`${config.authAPI}`, 'POST', JSON.stringify(requestPayload), undefined, false);
 }
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const AppName = 'BamServices';
 export default {
-  BASE_URL: 'http://localhost:9000/api/v1/',
+  BASE_URL: 'http://localhost:9000/api/v1',
 }
