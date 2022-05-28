49d96bffd6711a2b6ac750540440ac33e0529286
laraib-906 <laraibamna906@gmail.com>
2022-05-28 14:34:52 -0400

    updated config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const AppName = 'BamServices';
 export default {
-  BASE_URL: 'http://localhost:9000/api/v1',
+  BASE_URL_LOCAL: 'http://localhost:9000/api/v1',
+  BASE_URL: 'https://bamtell.herokuapp.com/api/v1',
   TOKEN_KEY: '_secret_text'
 }
